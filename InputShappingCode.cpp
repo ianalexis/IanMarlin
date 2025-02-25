3218ff6b03e5b0caf48a52b52b2b7ef4b77271da
Ian Bassi <ian.bassi@outlook.com>
2025-02-25 20:06:34 -0300

    Input Shapping independiente del step

--- InputShappingCode.cpp
+++ InputShappingCode.cpp
@@ -1,14 +1,14 @@
 ;AFTER_LAYER_CHANGE
 
 ;TestInicialHZ de 15 a 45
-;M593 F{(layer_num < 2 ? 0 : 15 + 30.0 * (layer_num - 2) / 297)} ; Hz Input Shaping Test
+;M593 F{(layer_num < 2 ? 0 : 15 + 30.0 * (layer_num - 2) / (total_layer_count-3))} ; Hz Input Shaping Test
 
 ;Test de microajuste segun resultado (Ej 36 a 41)
-;M593 F{(layer_num < 2 ? 0 : 36 + 5.0 * (layer_num - 2) / 297)} ; Hz Input Shaping Test
+;M593 F{(layer_num < 2 ? 0 : 36 + 5.0 * (layer_num - 2) / (total_layer_count-3))} ; Hz Input Shaping Test
 
 ;Test de dumping con resultado de microajuste
 ;M593 X F37
 ;M593 Y F37
-;M593 D{(layer_num < 2 ? 0 : 0 + 0.5 * (layer_num - 2) / 297)} ; Damping Test
+;M593 D{(layer_num < 2 ? 0 : 0 + 0.5 * (layer_num - 2) / (total_layer_count-3))} ; Damping Test
 
 ;{layer_z}
